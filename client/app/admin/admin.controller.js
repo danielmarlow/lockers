c2eab09da017bab85f727f14dfc92e4fce4f0694
Daniel Marlow <dan@danielmarlow.com>
2014-07-26 08:46:38 -0600

    Create admin route

---
+++ client/app/admin/admin.controller.js
@@ -0,0 +1,6 @@
+'use strict';
+
+angular.module('lockersApp')
+  .controller('AdminCtrl', function ($scope) {
+    $scope.message = 'Hello';
+  });
---
+++ client/app/admin/admin.js
@@ -0,0 +1,11 @@
+'use strict';
+
+angular.module('lockersApp')
+  .config(function ($stateProvider) {
+    $stateProvider
+      .state('admin', {
+        url: '/admin',
+        templateUrl: 'app/admin/admin.html',
+        controller: 'AdminCtrl'
+      });
+  });
