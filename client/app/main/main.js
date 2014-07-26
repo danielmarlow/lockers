39cc498fd6e57dc52bd0a0b8c980f43c68b16e76
Daniel Marlow <dan@danielmarlow.com>
2014-07-25 23:16:05 -0600

    Initial commit

---
+++ client/app/main/main.js
@@ -0,0 +1,11 @@
+'use strict';
+
+angular.module('lockersApp')
+  .config(function ($stateProvider) {
+    $stateProvider
+      .state('main', {
+        url: '/',
+        templateUrl: 'app/main/main.html',
+        controller: 'MainCtrl'
+      });
+  });
